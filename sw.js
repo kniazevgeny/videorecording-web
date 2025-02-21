5fb0d63d5b902b404515c4a90908bef67eefe99e
Evgeny Kniazev <eugene.kniazev@gmail.com>
2025-02-21 13:29:59 +0100

    upd: some pwa features

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'video-recorder-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://unpkg.com/@tailwindcss/browser@4'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
